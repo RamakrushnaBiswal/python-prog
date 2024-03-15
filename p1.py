1208f5213e88f5bbdcdff14f76e1db9ffd9d648f
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-03-15 13:50:52 +0530

    adedd

---
+++ p1.py
@@ -0,0 +1,14 @@
+def hello(a):
+    print('Hello',a)
+
+# Path: p2.py
+
+#renaming a module while importing it using 'as' keyword 
+# from import 
+#how it works:  
+# import module_name as new_name
+# module_name.function_name()
+# new_name.function_name()
+# module_name.variable_name
+
+# finding memebers of a module by using dir() function
---
+++ p2.py
@@ -0,0 +1,6 @@
+import p1
+
+b='bunty'
+p1.hello(b)
+
+print(dir(p1))
