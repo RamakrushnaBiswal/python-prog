af72c19051df3faacbc587577ca831c217e2d6f8
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-03-19 16:54:57 +0530

    added assignments

---
+++ test.py
@@ -0,0 +1,19 @@
+#start argument is optional an deafult start is 0
+#step argument is optional an deafult step is 1
+
+# from random import *
+# for i in range(10):
+    # print(randint(1,11))
+    #print('------------------>')
+    #print(randrange(1,11,2))
+#choice
+#it will not return random random numbers it will return random objects form the given list or tuple.
+
+from random import *
+
+list=['a','b','c','d','e']
+for i in range(10):
+    print(choice(list))
+
+# Encapsulation
+#it is a process of binding the data and the code that manipulates the data into a single unit.
